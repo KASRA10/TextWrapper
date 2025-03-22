40c60df5c65becdb6fd8182b6408574ea5a2a7b5
kasra10 <kasrasparrow@gmail.com>
2025-03-22 11:10:51 +0330

    complete Project

--- TextWrapper/TextWrapper/Form1.Designer.cs
+++ TextWrapper/TextWrapper/Form1.Designer.cs
@@ -32,98 +32,180 @@
 			this.Header_PNL = new System.Windows.Forms.Panel();
 			this.Header_LBL = new System.Windows.Forms.Label();
 			this.Footer_PNL = new System.Windows.Forms.Panel();
-			this.LinkedIn_IMG = new System.Windows.Forms.PictureBox();
-			this.GitHub_IMG = new System.Windows.Forms.PictureBox();
 			this.WebSite_IMG = new System.Windows.Forms.PictureBox();
+			this.GitHub_IMG = new System.Windows.Forms.PictureBox();
+			this.LinkedIn_IMG = new System.Windows.Forms.PictureBox();
+			this.SelectPath_BTN = new System.Windows.Forms.Button();
+			this.Path_TB = new System.Windows.Forms.TextBox();
+			this.TextBox_RTB = new System.Windows.Forms.RichTextBox();
+			this.Clear_BTN = new System.Windows.Forms.Button();
+			this.Run_BTN = new System.Windows.Forms.Button();
 			this.Header_PNL.SuspendLayout();
 			this.Footer_PNL.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.LinkedIn_IMG)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.GitHub_IMG)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.WebSite_IMG)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.GitHub_IMG)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.LinkedIn_IMG)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// Header_PNL
 			// 
-			this.Header_PNL.BackColor = System.Drawing.SystemColors.Window;
+			this.Header_PNL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Header_PNL.BackColor = System.Drawing.SystemColors.ControlLight;
 			this.Header_PNL.Controls.Add(this.Header_LBL);
-			this.Header_PNL.Location = new System.Drawing.Point(12, 12);
+			this.Header_PNL.Location = new System.Drawing.Point(9, 10);
+			this.Header_PNL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Header_PNL.Name = "Header_PNL";
-			this.Header_PNL.Size = new System.Drawing.Size(760, 50);
+			this.Header_PNL.Size = new System.Drawing.Size(764, 40);
 			this.Header_PNL.TabIndex = 0;
 			// 
 			// Header_LBL
 			// 
 			this.Header_LBL.AutoSize = true;
-			this.Header_LBL.Location = new System.Drawing.Point(312, 14);
+			this.Header_LBL.Location = new System.Drawing.Point(338, 11);
+			this.Header_LBL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.Header_LBL.Name = "Header_LBL";
-			this.Header_LBL.Size = new System.Drawing.Size(128, 21);
+			this.Header_LBL.Size = new System.Drawing.Size(89, 16);
 			this.Header_LBL.TabIndex = 0;
 			this.Header_LBL.Text = "Text Wrapper!";
 			// 
 			// Footer_PNL
 			// 
-			this.Footer_PNL.BackColor = System.Drawing.SystemColors.Window;
+			this.Footer_PNL.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.Footer_PNL.Controls.Add(this.WebSite_IMG);
 			this.Footer_PNL.Controls.Add(this.GitHub_IMG);
 			this.Footer_PNL.Controls.Add(this.LinkedIn_IMG);
-			this.Footer_PNL.Location = new System.Drawing.Point(12, 499);
+			this.Footer_PNL.Location = new System.Drawing.Point(9, 510);
+			this.Footer_PNL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.Footer_PNL.Name = "Footer_PNL";
-			this.Footer_PNL.Size = new System.Drawing.Size(760, 50);
+			this.Footer_PNL.Size = new System.Drawing.Size(764, 40);
 			this.Footer_PNL.TabIndex = 1;
 			// 
-			// LinkedIn_IMG
+			// WebSite_IMG
 			// 
-			this.LinkedIn_IMG.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.LinkedIn_IMG.Image = global::TextWrapper.Properties.Resources.linkedin;
-			this.LinkedIn_IMG.Location = new System.Drawing.Point(325, 11);
-			this.LinkedIn_IMG.Name = "LinkedIn_IMG";
-			this.LinkedIn_IMG.Size = new System.Drawing.Size(30, 30);
-			this.LinkedIn_IMG.TabIndex = 2;
-			this.LinkedIn_IMG.TabStop = false;
-			this.LinkedIn_IMG.Click += new System.EventHandler(this.LinkedIn_IMG_Click);
+			this.WebSite_IMG.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.WebSite_IMG.Image = global::TextWrapper.Properties.Resources.website;
+			this.WebSite_IMG.Location = new System.Drawing.Point(401, 5);
+			this.WebSite_IMG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.WebSite_IMG.Name = "WebSite_IMG";
+			this.WebSite_IMG.Size = new System.Drawing.Size(29, 30);
+			this.WebSite_IMG.TabIndex = 4;
+			this.WebSite_IMG.TabStop = false;
+			this.WebSite_IMG.Click += new System.EventHandler(this.WebSite_IMG_Click);
 			// 
 			// GitHub_IMG
 			// 
 			this.GitHub_IMG.Cursor = System.Windows.Forms.Cursors.Hand;
 			this.GitHub_IMG.Image = global::TextWrapper.Properties.Resources.gitHub;
-			this.GitHub_IMG.Location = new System.Drawing.Point(361, 11);
+			this.GitHub_IMG.Location = new System.Drawing.Point(367, 5);
+			this.GitHub_IMG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
 			this.GitHub_IMG.Name = "GitHub_IMG";
 			this.GitHub_IMG.Size = new System.Drawing.Size(30, 30);
 			this.GitHub_IMG.TabIndex = 3;
 			this.GitHub_IMG.TabStop = false;
 			this.GitHub_IMG.Click += new System.EventHandler(this.GitHub_IMG_Click);
 			// 
-			// WebSite_IMG
+			// LinkedIn_IMG
 			// 
-			this.WebSite_IMG.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.WebSite_IMG.Image = global::TextWrapper.Properties.Resources.website;
-			this.WebSite_IMG.Location = new System.Drawing.Point(397, 11);
-			this.WebSite_IMG.Name = "WebSite_IMG";
-			this.WebSite_IMG.Size = new System.Drawing.Size(30, 30);
-			this.WebSite_IMG.TabIndex = 4;
-			this.WebSite_IMG.TabStop = false;
-			this.WebSite_IMG.Click += new System.EventHandler(this.WebSite_IMG_Click);
+			this.LinkedIn_IMG.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.LinkedIn_IMG.Image = global::TextWrapper.Properties.Resources.linkedin;
+			this.LinkedIn_IMG.Location = new System.Drawing.Point(334, 5);
+			this.LinkedIn_IMG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.LinkedIn_IMG.Name = "LinkedIn_IMG";
+			this.LinkedIn_IMG.Size = new System.Drawing.Size(29, 30);
+			this.LinkedIn_IMG.TabIndex = 2;
+			this.LinkedIn_IMG.TabStop = false;
+			this.LinkedIn_IMG.Click += new System.EventHandler(this.LinkedIn_IMG_Click);
+			// 
+			// SelectPath_BTN
+			// 
+			this.SelectPath_BTN.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.SelectPath_BTN.Location = new System.Drawing.Point(9, 58);
+			this.SelectPath_BTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.SelectPath_BTN.Name = "SelectPath_BTN";
+			this.SelectPath_BTN.Size = new System.Drawing.Size(76, 24);
+			this.SelectPath_BTN.TabIndex = 2;
+			this.SelectPath_BTN.Text = "Save Path";
+			this.SelectPath_BTN.UseVisualStyleBackColor = false;
+			this.SelectPath_BTN.Click += new System.EventHandler(this.SelectPath_BTN_Click);
+			// 
+			// Path_TB
+			// 
+			this.Path_TB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Path_TB.BackColor = System.Drawing.SystemColors.Info;
+			this.Path_TB.Location = new System.Drawing.Point(89, 59);
+			this.Path_TB.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.Path_TB.Name = "Path_TB";
+			this.Path_TB.Size = new System.Drawing.Size(614, 23);
+			this.Path_TB.TabIndex = 3;
+			// 
+			// TextBox_RTB
+			// 
+			this.TextBox_RTB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TextBox_RTB.BackColor = System.Drawing.Color.Gainsboro;
+			this.TextBox_RTB.Location = new System.Drawing.Point(10, 88);
+			this.TextBox_RTB.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.TextBox_RTB.Name = "TextBox_RTB";
+			this.TextBox_RTB.Size = new System.Drawing.Size(763, 388);
+			this.TextBox_RTB.TabIndex = 4;
+			this.TextBox_RTB.Text = "";
+			// 
+			// Clear_BTN
+			// 
+			this.Clear_BTN.BackColor = System.Drawing.Color.SandyBrown;
+			this.Clear_BTN.Location = new System.Drawing.Point(707, 58);
+			this.Clear_BTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Clear_BTN.Name = "Clear_BTN";
+			this.Clear_BTN.Size = new System.Drawing.Size(66, 24);
+			this.Clear_BTN.TabIndex = 5;
+			this.Clear_BTN.Text = "Clear";
+			this.Clear_BTN.UseVisualStyleBackColor = false;
+			this.Clear_BTN.Click += new System.EventHandler(this.Clear_BTN_Click);
+			// 
+			// Run_BTN
+			// 
+			this.Run_BTN.BackColor = System.Drawing.Color.DarkSeaGreen;
+			this.Run_BTN.Location = new System.Drawing.Point(10, 482);
+			this.Run_BTN.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.Run_BTN.Name = "Run_BTN";
+			this.Run_BTN.Size = new System.Drawing.Size(763, 23);
+			this.Run_BTN.TabIndex = 6;
+			this.Run_BTN.Text = "Run";
+			this.Run_BTN.UseVisualStyleBackColor = false;
+			this.Run_BTN.Click += new System.EventHandler(this.Run_BTN_Click);
 			// 
 			// WorkSpace_FRM
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.BackColor = System.Drawing.Color.DimGray;
 			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.Run_BTN);
+			this.Controls.Add(this.Clear_BTN);
+			this.Controls.Add(this.TextBox_RTB);
+			this.Controls.Add(this.Path_TB);
+			this.Controls.Add(this.SelectPath_BTN);
 			this.Controls.Add(this.Footer_PNL);
 			this.Controls.Add(this.Header_PNL);
-			this.Font = new System.Drawing.Font("Montserrat Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.Font = new System.Drawing.Font("Microsoft PhagsPa", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MaximizeBox = false;
 			this.Name = "WorkSpace_FRM";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Work Space";
 			this.Header_PNL.ResumeLayout(false);
 			this.Header_PNL.PerformLayout();
 			this.Footer_PNL.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.LinkedIn_IMG)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.GitHub_IMG)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.WebSite_IMG)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.GitHub_IMG)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.LinkedIn_IMG)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -135,6 +217,11 @@
 		private System.Windows.Forms.PictureBox WebSite_IMG;
 		private System.Windows.Forms.PictureBox GitHub_IMG;
 		private System.Windows.Forms.PictureBox LinkedIn_IMG;
+		private System.Windows.Forms.Button SelectPath_BTN;
+		private System.Windows.Forms.TextBox Path_TB;
+		private System.Windows.Forms.RichTextBox TextBox_RTB;
+		private System.Windows.Forms.Button Clear_BTN;
+		private System.Windows.Forms.Button Run_BTN;
 	}
 }
 
--- TextWrapper/TextWrapper/Form1.cs
+++ TextWrapper/TextWrapper/Form1.cs
@@ -1,15 +1,71 @@
-﻿using System;
+﻿// Ignore Spelling: FRM splitted
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace TextWrapper
 {
 	public partial class WorkSpace_FRM : Form
 	{
+		#region Public_Variables
+		public List<string> splittedText = new List<string>();
+		#endregion
 		public WorkSpace_FRM()
 		{
 			InitializeComponent();
+			Path_TB.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+			TextBox_RTB.Text = "Enter Your Text Here . . .";
 		}
 
+		// Select Path
+		private void SelectPath_BTN_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog SaveFolderPath = new FolderBrowserDialog();
+			SaveFolderPath.RootFolder = Environment.SpecialFolder.MyComputer;
+			SaveFolderPath.ShowNewFolderButton = true;
+			if (SaveFolderPath.ShowDialog() == DialogResult.OK)
+			{
+				Path_TB.Text = SaveFolderPath.SelectedPath; // Set Selected Path Address To The Text Of TextBox. 
+			}
+		}
+
+		// Run Button
+		private void Run_BTN_Click(object sender, EventArgs e)
+		{
+			splittedText = TextBox_RTB.Text.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+			try
+			{
+				// Get the folder path from Path_TB TextBox and add a filename
+				string folderPath = Path_TB.Text;
+				string fileName = "TextWrappedResult.txt";
+				string filePath = Path.Combine(folderPath, fileName);
+
+				// Write all strings to the file, each on a new line
+				File.WriteAllLines(filePath, splittedText);
+
+				// Show success message with the full path
+				DialogResult result = MessageBox.Show($"File saved successfully at: {filePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				// Handle any errors (like invalid path, permissions, etc.)
+				DialogResult result = MessageBox.Show($"Error saving file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		// Clear Button
+		private void Clear_BTN_Click(object sender, EventArgs e)
+		{
+			splittedText.Clear();
+			Path_TB.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+			TextBox_RTB.Text = "Enter Your Text Here . . .";
+		}
+
+		#region Footer
 		// LinkedIn Image Link
 		private void LinkedIn_IMG_Click(object sender, EventArgs e)
 		{
@@ -27,7 +83,7 @@ namespace TextWrapper
 			catch (Exception ex)
 			{
 				// Handle exceptions, such as when a browser is unavailable  
-				MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				DialogResult result = MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 
@@ -48,7 +104,7 @@ namespace TextWrapper
 			catch (Exception ex)
 			{
 				// Handle exceptions, such as when a browser is unavailable  
-				MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				DialogResult result = MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 
@@ -69,8 +125,10 @@ namespace TextWrapper
 			catch (Exception ex)
 			{
 				// Handle exceptions, such as when a browser is unavailable  
-				MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				DialogResult result = MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
+
+		#endregion
 	}
 }
